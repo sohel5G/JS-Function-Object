be9bd7bfda045d4ea8241dea340752029c68ecd1
sohel5G <contactsohelrana>
2023-08-04 00:47:22 +0600

    some task done

--- task.js
+++ task.js
@@ -1,9 +1,67 @@
-let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let anotherArray = myArray.slice(2, 4);
+// console.log(anotherArray);
 
-myArray.slice(2, 4);
-// console.log(myArray);
+// let haveIt = myArray.includes(55);
+// console.log(haveIt);
 
-let haveIt = myArray.includes(55);
-console.log(haveIt);
+// let theName = 'Hello World !';
+// result = theName.substring(1, 4);
+// console.log(result);
+// console.log(theName[0])
+
+// let myParseInt = 120.876;
+// let x= parseInt(myParseInt);
+// console.log(x);
+// console.log(typeof x);
+
+
+/*
+
+let a = '23.345';
+console.log(a);
+let b = parseInt(a);
+let c = b.toFixed(2)
+console.log(b);
+console.log(c);
+
+console.log(parseFloat(c));
+
+console.log('_________________________');
+
+let y = '23.345';
+console.log(y);
+let z = parseFloat(y);
+console.log(z);
+
+*/
+
+/*
+function fOne (money, taka){
+    return money + taka;
+}
+let varName = fOne(20, 50);
+console.log(varName);
+*/
+
+/*
+function gorMark (as1, as2, as3) {
+    let total = as1 + as2 + as3;
+    let avg = total / 3;
+    return avg;
+}
+let gorAvg = gorMark(60, 51, 40);
+console.log(gorAvg);
+*/
+
+
+function squire (number) {
+    let result = number * number;
+    return result;
+}
+console.log(squire(9));
+
+
+let myComputer 
 
 
